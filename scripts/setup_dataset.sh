1a6d0cdd85ac7e5b7338f18298bf5faf56a15e4b
ghnmqdtg <ghnmqdtg@gmail.com>
2023-05-16 13:46:19 +0800

    Misc: Update shell scripts to setup the datasets

--- scripts/setup_dataset.sh
+++ scripts/setup_dataset.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
 echo "Step 1: Download Cityscapes Dataset"
-# Login to Cityscapes, you should provide your username and password
+echo "1-1: Login to Cityscapes, you should provide your username and password"
 wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username={your_username}&password={your_password}&submit=Login' https://www.cityscapes-dataset.com/login/
-# Download `gtFine_trainvaltest.zip` with packageID 1
+echo "1-2: Download `gtFine_trainvaltest.zip` with packageID 1"
 wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1
-# Download `leftImg8bit_trainvaltest.zip` with packageID 3
+echo "1-3: Download `leftImg8bit_trainvaltest.zip` with packageID 3"
 wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3
-
+echo "1-4: Unzip the downloaded files"
+unzip gtFine_trainvaltest.zip
+unzip leftImg8bit_trainvaltest.zip
 
 echo "Step 2: Setup Cityscapes Dataset"
-# Create folders that `cityscapes-to-coco-conversion` prefer
+echo "2-1: Create folders that `cityscapes-to-coco-conversion` prefer"
 mkdir -p cityscapes-to-coco-conversion/data/cityscapes/annotations
-# Move downloaded datasets to required path
+echo "2-2: Move downloaded datasets to required path"
 mv gtFine cityscapes-to-coco-conversion/data/cityscapes
 mv leftImg8bit cityscapes-to-coco-conversion/data/cityscapes
-# Run the conversion
-echo "Running the conversion to get bbox (it takes some time) ..."
+echo "Running the conversion to get COCO annotations and bbox (it takes some time) ..."
 python main.py --dataset cityscapes --datadir cityscapes-to-coco-conversion/data/cityscapes --outdir cityscapes-to-coco-conversion/data/cityscapes/annotations
 
 echo "Step 3: Convert Cityscapes Dataset into YOLO format"
 # Change back to the previous directory
-# Create a new subfolder
+echo "3-1: Create a new subfolder"
 mkdir -p yolov7/customdata/images
-# Copy leftImg8bit to yolov7 folder
+echo "3-2: Copy the images and annotations to the yolov7 subfolder"
 cp -r cityscapes-to-coco-conversion/data/cityscapes/leftImg8bit/* yolov7/customdata/images
 cp cityscapes-to-coco-conversion/data/cityscapes/annotations/instancesonly_filtered_gtFine_train.json yolov7/customdata/images/train/_annotations.coco.json
 cp cityscapes-to-coco-conversion/data/cityscapes/annotations/instancesonly_filtered_gtFine_val.json yolov7/customdata/images/val/_annotations.coco.json
 # Run the format converter
-echo "Running the format converter..."
+echo "3-3: Running the COCO to YOLO format converter..."
 python ./scripts/dataset_format_converter.py --source-folder-path ./yolov7/customdata/images/
 
 echo "Process complete."
--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -8,9 +8,9 @@ echo "Install torch and CUDA"
 pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
 # The VSCdoe interpreter path is ~/.conda/envs/yolo_with_cropping/bin/python
 
-# echo "Clone the YOLOv7"
-git clone https://github.com/WongKinYiu/yolov7.git
 echo "Install the required packages"
 pip install -r yolov7/requirements.txt
+pip install -r cityscapes-to-coco-conversion/requirements.txt
+pip install wandb
 pip install pycocotools
 pip install torchmetrics
